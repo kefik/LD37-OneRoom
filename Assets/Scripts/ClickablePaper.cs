2a7f8ba29459726d3dabaaaea7fbadc420e4c7e3
Ondřej Paška <on.paska@gmail.com>
2016-12-11 18:25:17 +0100

    crumpled hint system

---
+++ Assets/Scripts/ClickablePaper.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickablePaper : MonoBehaviour {
+
+    public Transform unrolledPaper;
+
+    // Use this for initialization
+	void Start () {
+	
+	}
+
+    void OnMouseDown()
+    {
+        print("clicked");
+        Instantiate(unrolledPaper, this.transform.parent.position, Quaternion.identity);
+        
+        // this object was clicked - do something
+        Destroy(this.gameObject);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
